ad6a9a7c711dea18e111102edcc38ca7fc444e0a
Informatica <informatica@alumno.local>
2024-01-18 17:29:24 -0600

    Se avanzo en el dibujo del ahorcado y se hicieron modificaciones en los diferentes archivos

--- Ahorcado.js
+++ Ahorcado.js
@@ -1,19 +1,32 @@
-var palabras=[Casa, Perro, Gato, Chihuahua];
+var palabras = [Casa, Perro, Gato, Chihuahua];
 var botonStart = document.getElementById("BotonStart");
 var contador = document.getElementById("Contador");
-var lienzo = document.getElementById("DibujoAhorcado");
+var canvas = document.getElementById("pitbull");
+var dibujo = canvas.getContext("2d");
 var mistakes = 0;
 
-function EmpezarJuego(){
-    DibujarMono();
-}
-
-function DibujarMono(){
-    dibujo = lienzo.getContext('2d');
-    dibujo = lineWidth=1;
-    
-
-}
 
 
-botonStart.addEventListener('click', EmpezarJuego());
+//function EmpezarJuego(){
+//}
+
+
+
+//function DibujarMono() {
+    //Cabeza
+    dibujo.beginPath();
+    dibujo.arc(200, 200, 50, 0, Math.PI * 2, true);
+    //Cuerpo
+    dibujo.lineWidth = 1;
+    dibujo.moveTo(150, 150);
+    dibujo.lineTo(100, 200);
+    //Brazo Izq
+    dibujo.moveTo(150,150);
+    dibujo.lineTo(90,150);
+    dibujo.stroke();
+
+
+//}
+
+
+//botonStart.addEventListener('click', EmpezarJuego());
